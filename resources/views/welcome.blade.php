dd5fe7a83aedd5f6ffc5267a4308c6d20a67210b
MatteoStrazzera <mattestrazz@gmail.com>
2024-05-09 16:52:40 +0200

    Print in page basic cards with database info

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,4 +4,31 @@
     <div class="container">
         <h1>Movies cards</h1>
     </div>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            @foreach ($movies as $movie)
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="card-title">
+                                {{ $movie->title }}
+                            </h3>
+                            <p>
+                                {{ $movie->original_title }}
+                            </p>
+                            <p>
+                                {{ $movie->nationality }}
+                            </p>
+                            <p>
+                                {{ $movie->date }}
+                            </p>
+                            <p>
+                                {{ $movie->vote }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
